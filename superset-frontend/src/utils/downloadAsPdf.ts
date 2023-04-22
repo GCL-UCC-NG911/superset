63c8e808df6bdca60c8371b1f1e7e206ba53ef0b
Rodrigo Pastro <rodrigo.pastro@atos.net>
2023-04-22 16:47:01 -0400

    lint

--- superset-frontend/src/utils/downloadAsPdf.ts
+++ superset-frontend/src/utils/downloadAsPdf.ts
@@ -62,7 +62,7 @@ const generatePdf = (canvas: HTMLCanvasElement, filename: string) => {
   const pageHeightPx = Math.floor(canvasWidthPx * pageRatio);
 
   // Calculate the number of pages
-  let nPages = Math.ceil(canvasHeightPx / pageHeightPx);
+  const nPages = Math.ceil(canvasHeightPx / pageHeightPx);
 
   let adjustedCanvas;
   if (canvasHeightPx % pageHeightPx !== 0) {
