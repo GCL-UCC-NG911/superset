2ea4c243943e49547beec1664eff9fe49b0a08ea
Eduardo Sanday <eduardo.sanday.external@atos.net>
2023-07-04 08:18:15 -0300

    commit 64

--- superset/dashboards/api.py
+++ superset/dashboards/api.py
@@ -996,8 +996,10 @@ class DashboardRestApi(BaseSupersetModelRestApi):
 
         # If force, request a screenshot from the workers
         current_user = get_current_user()
-        DashboardChartScreenshot(current_user, json.loads(request.form["form_data"]), pk).print()
+        DashboardChartScreenshot(current_user, json.loads(request.form["formData"]), pk).print()
         # fetch the dashboard screenshot using the current user and cache if set
+        if json_body.form_data:
+          self.incr_stats("from_cache", self.dashboarddownload.__name__)
         if json_body.formData:
           # for element in json_body.formData: # loop 
             # if element.type == "CHART":
