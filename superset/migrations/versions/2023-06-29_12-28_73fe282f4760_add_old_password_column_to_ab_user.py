2233459ce6ec9f165ee887f3e8c2b028fc6c2d79
LUANA POVROZNIK (ext) <luana.povroznik.external@atos.net>
2023-07-04 10:49:28 -0300

    rename method

--- superset/migrations/versions/2023-06-29_12-28_73fe282f4760_add_old_password_column_to_ab_user.py
+++ superset/migrations/versions/2023-06-29_12-28_73fe282f4760_add_old_password_column_to_ab_user.py
@@ -31,13 +31,13 @@ import sqlalchemy as sa
 from sqlalchemy import engine_from_config
 from sqlalchemy.engine import reflection
 
-def table_has_column(table, column):
+def column_verification(table, column):
     config = op.get_context().config
     engine = engine_from_config(
         config.get_section(config.config_ini_section), prefix='sqlalchemy.')
-    insp = reflection.Inspector.from_engine(engine)
+    inspector = reflection.Inspector.from_engine(engine)
     has_column = False
-    for col in insp.get_columns(table):
+    for col in inspector.get_columns(table):
         if column not in col['name']:
             continue
         has_column = True
@@ -45,7 +45,7 @@ def table_has_column(table, column):
 
 def upgrade():
     with op.batch_alter_table("ab_user") as batch_op:
-        if not table_has_column("ab_user", "password_history"):
+        if not column_verification("ab_user", "password_history"):
             batch_op.add_column(sa.Column("password_history", sa.String(256), nullable=True))
 
 def downgrade():
