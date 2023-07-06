fdb06af7ba2db458790cb80af78a07ef2da19fa1
Eduardo Sanday <eduardo.sanday.external@atos.net>
2023-07-06 10:06:01 -0300

    commit 83

--- superset/utils/screenshots.py
+++ superset/utils/screenshots.py
@@ -423,7 +423,7 @@ class BaseChartScreenshot:
         return new_img.read()
 
     def print(self):
-        logger.info("commit 81")
+        logger.info("commit 83")
         logger.info("##### User: [%s], json: [%s], pk: [%s], digest: [%s]", str(self.user), str(self.json), str(self.pk), str(self.digest))
 
 class ChartScreenshot(BaseScreenshot):
