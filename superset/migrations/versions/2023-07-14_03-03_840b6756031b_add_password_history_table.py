c5deee262ef065e8693bf6c98d673bf7938e53d3
LUANA POVROZNIK (ext) <luana.povroznik.external@atos.net>
2023-07-26 11:53:25 -0300

    updates

--- superset/migrations/versions/2023-07-14_03-03_840b6756031b_add_password_history_table.py
+++ superset/migrations/versions/2023-07-14_03-03_840b6756031b_add_password_history_table.py
@@ -28,20 +28,35 @@ down_revision = 'f3c2d8ec8595'
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import engine_from_config
+from sqlalchemy.engine import reflection
+
+def table_verification(table):
+    config = op.get_context().config
+    engine = engine_from_config(
+        config.get_section(config.config_ini_section), prefix="sqlalchemy."
+    )
+    inspector = reflection.Inspector.from_engine(engine)
+    tables = inspector.get_table_names()
+    has_table = False
+    if table in tables:
+        has_table = True
+    return has_table
 
 def upgrade():
-    op.create_table(
-        "password_history",
-        sa.Column("id", sa.Integer(), nullable=False),
-        sa.Column("old_password", sa.String(256), nullable=True),
-        sa.Column("timestamp", sa.DateTime(), nullable=True),
-        sa.Column("user_id", sa.Integer(), nullable=True),
-        sa.ForeignKeyConstraint(
-            ["user_id"],
-            ["ab_user.id"],
-        ),
-        sa.PrimaryKeyConstraint("id"),
-    )
+    if not table_verification("password_history"):
+        op.create_table(
+            "password_history",
+            sa.Column("id", sa.Integer(), nullable=False),
+            sa.Column("old_password", sa.String(256), nullable=True),
+            sa.Column("timestamp", sa.DateTime(), nullable=True),
+            sa.Column("user_id", sa.Integer(), nullable=True),
+            sa.ForeignKeyConstraint(
+                ["user_id"],
+                ["ab_user.id"],
+            ),
+            sa.PrimaryKeyConstraint("id"),
+        )
 
 def downgrade():
-    op.drop_table("password_history")
+    pass
