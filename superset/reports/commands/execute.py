8c737b615d69a8e5b556ba39957ad45c2ae9135b
Eduardo Sanday <eduardo.sanday.external@atos.net>
2023-07-31 12:15:42 -0300

    commit 319

--- superset/reports/commands/execute.py
+++ superset/reports/commands/execute.py
@@ -401,7 +401,7 @@ class BaseReportState:
 
         alltables = []
         for children in childrenElement['children']:
-            tables = self.getAllTables(props, slices, children)
+            tables = self.getAllTables(props, slices, filters, children)
             # logger.info(tables)
             for table in tables:
                 alltables.append(table)
