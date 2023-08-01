94cb1eb27b80b454bc11adb6f02f19e484e5adfa
Luana Povroznik <111379423+lpovroznik@users.noreply.github.com>
2023-08-01 09:59:39 -0300

    NGLS-3086: Security - Add password_history table (#14)
    * adding new column to ab_user model
    
    * change column name
    
    * add try except to migration script
    
    * add column verification to migration script
    
    * rename method
    
    * update password history model
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates

---
+++ superset/migrations/versions/2023-07-14_03-03_840b6756031b_add_password_history_table.py
@@ -0,0 +1,57 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""add_password_history_table
+
+Revision ID: 840b6756031b
+Revises: f3c2d8ec8595
+Create Date: 2023-07-14 03:03:11.900092
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '840b6756031b'
+down_revision = 'f3c2d8ec8595'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import engine_from_config
+from sqlalchemy.engine import reflection
+
+def table_verification(table):
+    config = op.get_context().config
+    engine = engine_from_config(
+        config.get_section(config.config_ini_section), prefix="sqlalchemy."
+    )
+    inspector = reflection.Inspector.from_engine(engine)
+    has_table = False
+    if table in inspector.get_table_names():
+        has_table = True
+    return has_table
+
+def upgrade():
+    if not table_verification("password_history"):
+        op.create_table(
+            "password_history",
+            sa.Column("id", sa.Integer(), nullable=False),
+            sa.Column("old_password", sa.String(256), nullable=True),
+            sa.Column("timestamp", sa.DateTime(), nullable=True),
+            sa.Column("user_id", sa.Integer(), sa.ForeignKey("ab_user.id"), nullable=True),
+            sa.PrimaryKeyConstraint("id"),
+        )
+
+def downgrade():
+    op.drop_table("password_history")
