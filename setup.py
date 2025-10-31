c94140ac1071322fb95f853d47e951e11184ba30
Eduardo Sanday <eduardo.sanday.external@atos.net>
2025-10-31 19:14:40 -0300

    test

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ with open(VERSION_INFO_FILE, "w") as version_file:
 version_string = version_string.replace("-dev", ".dev0")
 
 setup(
+    name="apache-superset",
+    description="A modern, enterprise-ready business intelligence web application",
     version=version_string,
     packages=find_packages(),
     include_package_data=True,
